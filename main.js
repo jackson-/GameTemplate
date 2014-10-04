5688f3b8c26d3a32c544d654146d2c271fff6089
jackson- <jackson.t.devin@gmail.com>
2014-10-03 23:50:39 -0400

    added Aiyo and tested powers array

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function THEM(firstName, lastName, superName, powers) {
+function THEM(firstName, lastName, superName) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.superName = superName;
 	this.powers = [];
 }
+
+var Aiyo = new THEM("Mason", "Pryce", "Aiyo");
+
+Aiyo.powers.push("inorganic material creation, organic creation, palm vortex")
+
+console.log(Aiyo.powers)
