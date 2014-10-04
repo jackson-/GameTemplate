f60ac809c69f34f8482ea57b35dc3adea1ebf123
jackson- <jackson.t.devin@gmail.com>
2014-10-04 00:27:19 -0400

    Made powers into an object literal inside THEM class

--- main.js
+++ main.js
@@ -2,11 +2,25 @@ function THEM(firstName, lastName, superName) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.superName = superName;
-	this.powers = [];
+	this.powers = {};
 }
 
 var Aiyo = new THEM("Mason", "Pryce", "Aiyo");
 
-Aiyo.powers.push("inorganic material creation, organic creation, palm vortex")
+Aiyo.powers.fakeCreate = "inorganic creation";
+Aiyo.powers.lifeCreate = function (){
+	console.log(Aiyo.powers)
+};
 
-console.log(Aiyo.powers)
+
+
+var Demiurge = new THEM("Lysanthe", "Menzine", "Demiurge");
+$(document).ready(function(){
+
+Aiyo.powers.lifeCreate();
+
+
+});
+
+
+
