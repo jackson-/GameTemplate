2f888be970726f0dfda32fde9f791a2fd637dafe
jackson- <jackson.t.devin@gmail.com>
2014-10-04 00:33:48 -0400

    comments

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-function THEM(firstName, lastName, superName, powers) {
+//this creates superhero class for all THEM's
+function THEM(firstName, lastName, superName) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.superName = superName;
-	this.powers = [];
+	this.powers = {};
 }
+//instantiate new hero
+var Aiyo = new THEM("Mason", "Pryce", "Aiyo");
+//add abilities to powers associative array
+Aiyo.powers.fakeCreate = "inorganic creation";
+Aiyo.powers.lifeCreate = function (){
+	console.log(Aiyo.powers)
+};
+
+var Demiurge = new THEM("Lysanthe", "Menzine", "Demiurge");
+
+$(document).ready(function(){
+
+Aiyo.powers.lifeCreate();
+
+
+});
