7728adc97512f7905a20b5bf5a0831e27e40e5b6
jackson- <jackson.t.devin@gmail.com>
2014-10-04 01:01:23 -0400

    Demiurge powers added

--- main.js
+++ main.js
@@ -5,22 +5,46 @@ function THEM(firstName, lastName, superName) {
 	this.superName = superName;
 	this.powers = {};
 }
-//instantiate new hero
+//instantiate new hero:Aiyo
 var Aiyo = new THEM("Mason", "Pryce", "Aiyo");
 //add abilities to powers associative array
 Aiyo.powers.fakeCreate = "inorganic creation";
 Aiyo.powers.lifeCreate = function (){
 	console.log(Aiyo.powers)
 };
-
+//instantiate new hero:Demiurge
 var Demiurge = new THEM("Lysanthe", "Menzine", "Demiurge");
-
+//add Demiurge abilities
+Demiurge.powers.flight = function(direction, distance){
+	this.direction = direction;
+	this.position = direction + distance;
+};
+Demiurge.powers.battle = function(targetEnemy, attack){
+	if (attack == "punch"){
+	this.targetEnemy.life -= 30
+	} else if (attack == "kick"){
+	this.targetEnemy.life -= 20
+	} else if (attack == "slam"){
+	this.targetEnemy.life -= 50
+	}
+};
+Demiurge.powers.shoot = function(targetEnemy, specialBullet){
+	if(typeof(specialBullet)==='undefined') specialBullet = none;
+	this.targetEnemy = targetEnemy;
+	if(hit == true){
+	targetEnemy.life -= 30;
+	} 
+};
+//instantiate new hero:Spado
+var Spado = new THEM("Joseph", "Spades", "Spado");
+//add Spado abilities
+Spado.powers.shadowBurst = "shoot out a burst of shadows";
+Spado.powers.shadowForm = function(direction, shape){
+	this.direction = direction;
+	this.shadowShape = shape;
+};
 $(document).ready(function(){
 
-Aiyo.powers.lifeCreate();
-
+console.log(Demiurge.powers.shoot);
 
 });
-
-
-
